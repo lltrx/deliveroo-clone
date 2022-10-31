ffcecf9f79ce4e9dbe0309054f09383954eeee90
@2614579a <2614579a@student.gla.ac.uk>
2022-10-31 13:51:33 +0000

    Setup the api but got an error in HomeScreen fetch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,33 @@
 import { View, Text, Image, SafeAreaView, TextInput, ScrollView} from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { ChevronDownIcon, UserIcon ,AdjustmentsVerticalIcon, MagnifyingGlassIcon} from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories ] = useState([])
 
   useLayoutEffect(() => {
       navigation.setOptions({
         headerShown: false,
       })
   }, []);
+
+useEffect(() => {
+  sanityClient.fetch(
+    `*[_type == "featured"]{
+      ...,
+      restaurants[]->{
+        ...,
+        dishes[]->
+      }
+    }`).then((data) => {
+      setFeaturedCategories(data)
+    });
+}, []);
 
   return (
     <SafeAreaView className="bg-white pt-5">
